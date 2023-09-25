530b8af8a4186e8117324043251bc9da0ff70e1e
dlwlckdWkd12 <jccc23@naver.com>
2023-09-25 21:16:18 +0900

    spring_vue_setting

---
+++ backend/src/test/java/org/spring/springVue/SpringVueApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.springVue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
